b1a139ad562cf6d75fc2fb99de6575ce18c8777a
ashutosh-dev-tech <ay839065@gmail.com>
2021-05-14 19:05:07 +0530

    added interview card display

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import "./Box";
+import "./box.css";
+import allCards from '../../experiences/allExp.json';
+import Card from '../card/Card';
+
 const Box = () => (
     <div className='container'>
+        {(allCards.expList).map((curExp, ind)=>{
+        return <Card curExp={curExp} ind={ind}/>
+    })}
     </div>
-)
+);
 
 export default Box;
