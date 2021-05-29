dd7698ad4495eb4d8bae851d43831f0209e05ab0
ashutosh-dev-tech <ay839065@gmail.com>
2021-05-29 19:58:19 +0530

    more exp added

--- src/pages/curPost/CurPost.js
+++ src/pages/curPost/CurPost.js
@@ -4,7 +4,6 @@ import {useParams} from 'react-router-dom';
 
 const CurPost = (props) => {
     const params = useParams();
-    console.log('from cur post: ', params);
     return (
         <Post id={params.postId}/>
     );
