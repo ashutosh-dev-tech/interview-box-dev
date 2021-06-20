5451134080bd8a6ea608b6cbdafb9a128e53f6a1
ashutosh-dev-tech <ay839065@gmail.com>
2021-06-20 22:54:59 +0530

    feat: general improvements

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import "./box.css";
 import Card from '../card/Card';
-import { Link } from 'react-router-dom';
 
 const Box = (props) => {
     return (
         <div className='container'>
             {(props.allCards.expList).map((curExp, ind)=>{
-            return ( 
-                <Link to={"/"+ind}>
+                return (                 
                     <Card key={ind} curExp={curExp}/>
-                </Link>
-            );
-        })}
+                );
+            })}
         </div>
     );
 }
